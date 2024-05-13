01edf2af10c9e3ec817125bfe95791578ef67d2a
Your Name <megaacc237@example.com>
2024-05-13 16:22:04 +0630

    added js file and note by developer1

---
+++ script.js
@@ -0,0 +1,11 @@
+ /* 
+git မှာတင်ဖို့ အတွက် repo အသစ်လုပ်။ပြီးရင် repo နာမည်ပေး။ repo နာမည်နဲ့  ကိုယ်တင်မယ် folder နာမည်ကို တူတူပေးတာကောင်းတယ်။
+ပြီးရင် ကိုယ့်ရဲ့ repo link ကို git မှာ သုံးရတာလွယ်အောင် နာမည်ပေး။ github မှာ နည်းတွေပြထားတယ်။
+
+**ကိုယ့်ရဲ့ code တွေတင်နည်း
+    git push နဲ့တင်ရမယ်။
+    ရေးနည်းကဒီလို
+    git push <repoName> <branchName>
+    git မှာတင်ထားတဲ့ branch ကိုဖျက်နည်း။
+    git push <repeName> --delete <branchName>
+    */
